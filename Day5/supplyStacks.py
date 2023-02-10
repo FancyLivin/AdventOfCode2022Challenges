3d67359b17e2c20c9be3a8ef86af0b830bd8ea47
Yamaan Khan <ykhan4@asu.edu>
2023-02-09 17:28:13 -0700

    Creating Day 5 files

---
+++ Day5/supplyStacks.py
@@ -0,0 +1,12 @@
+##### PART 1 #####
+def stackSortOne():
+    file = open("supplyStacks.txt", "r")
+
+    print(file)
+    return 0
+
+##### PART 2 #####
+
+
+##### PART 1/2 OUTPUT #####
+print(stackSortOne())
