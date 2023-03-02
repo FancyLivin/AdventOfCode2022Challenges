f02a7a555e0169738161a15343352701883a3113
Yamaan Khan <ykhan4@asu.edu>
2023-03-02 12:39:19 -0700

    Added comments to Day 4 code

--- Day4/campCleanup.py
+++ Day4/campCleanup.py
@@ -5,15 +5,18 @@ def cleanupOne():
     fullyContained = 0
 
     for x in file:
+        # Creating two elf arrays for future logic
         sections = x.split(",")
         elfOne = sections[0].split("-")
         elfTwo = sections[1].split("-")
         
+        # Logic for finding fully overlapping individuals
         if int(elfOne[0]) >= int(elfTwo[0]) and int(elfOne[1]) <= int(elfTwo[1]):
             fullyContained += 1
         elif int(elfTwo[0]) >= int(elfOne[0]) and int(elfTwo[1]) <= int(elfOne[1]):
             fullyContained += 1
 
+    file.close()
     return fullyContained
 
 ##### PART 2 #####
@@ -23,10 +26,12 @@ def cleanupTwo():
     partiallyContained = 0
 
     for x in file:
+        # Creating two elf arrays for future logic
         sections = x.split(",")
         elfOne = sections[0].split("-")
         elfTwo = sections[1].split("-")
 
+        # Logic for finding partially overlapping individuals
         if int(elfTwo[0]) <= int(elfOne[0]) <= int(elfTwo[1]):
             partiallyContained += 1
         elif int(elfTwo[0]) <= int(elfOne[1]) <= int(elfTwo[1]):
@@ -36,6 +41,7 @@ def cleanupTwo():
         elif int(elfOne[0]) <= int(elfTwo[1]) <= int(elfOne[1]):
             partiallyContained += 1
 
+    file.close()
     return partiallyContained
 
 ##### PART 1/2 OUTPUT #####
