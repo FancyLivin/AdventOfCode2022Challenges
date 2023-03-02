b9fb309b57fa4a4e389111f48049940f9f97dadc
Yamaan Khan <ykhan4@asu.edu>
2023-03-02 12:12:36 -0700

    Condensing Day 5 code

--- Day5/supplyStacks.py
+++ Day5/supplyStacks.py
@@ -1,7 +1,10 @@
-##### PART 1 #####
-def stackSortOne():
+##### PART 1/2 #####
+# Return output of part 1 if currentTask == 0
+# Return output of part 2 if currentTask != 0
+def stackSort(currentTask):
     file = open("supplyStacks.txt", "r")
     
+    # Finding out total number of stacks
     tempString = file.readline()
     totalStacks = len(tempString)//4
     nestedStack = [[]*totalStacks for iterator in range(totalStacks)]
@@ -22,7 +25,10 @@ def stackSortOne():
             # Logic for stack movement
             for iterator in range(totalMoves):
                 if len(nestedStack[startStack]) != 0:
-                    val = nestedStack[startStack].pop(0)
+                    if currentTask == 0: # Part 1 Logic
+                        val = nestedStack[startStack].pop(0)
+                    else: # Part 2 Logic
+                        val = nestedStack[startStack].pop(totalMoves - iterator - 1)
                     nestedStack[destStack].insert(0, val)
             
         elif len(x) > 1:
@@ -35,75 +41,19 @@ def stackSortOne():
                 # 1, 5, 9 store values (every 4 stores value)
                 for count, char in enumerate(x):
                     if count % 4 == 1:
-                        if char == ' ':
-                            None
-                        else:
+                        if char != ' ':
                             nestedStack[currentStack].append(char)
                         currentStack += 1
-
-                None
     
+    # Getting top character of each stack
     stackOutput = ''
     for x in range(len(nestedStack)):
         if len(nestedStack[x]) != 0:
             stackOutput += nestedStack[x][0]
-    return stackOutput
-
-    # return nestedStack
-
-##### PART 2 #####
-def stackSortTwo():
-    file = open("supplyStacks.txt", "r")
-    
-    tempString = file.readline()
-    totalStacks = len(tempString)//4
-    nestedStack = [[]*totalStacks for iterator in range(totalStacks)]
 
     file.close()
-
-    file = open("supplyStacks.txt", "r")
-
-    for x in file:
-        if x[:5] == 'move ':
-            # Command to grab from text file to move objects to new stack
-            command = x[5:]
-            # Splits the command into its 3 separate values
-            totalMoves = int(command.partition('from')[0])
-            startStack = int(command.partition('to')[0].partition('from')[2]) - 1
-            destStack = int(command.partition('to')[2]) - 1
-
-            # Logic for stack movement
-            for iterator in range(totalMoves):
-                if len(nestedStack[startStack]) != 0 :
-                    val = nestedStack[startStack].pop(totalMoves - iterator - 1)
-                    nestedStack[destStack].insert(0, val)
-            
-        elif len(x) > 1:
-            # When reading each stack's number
-            if x[1] == '1':
-                None
-            # Storing values in appropriate stacks
-            else:
-                currentStack = 0
-                # 1, 5, 9 store values (every 4 stores value)
-                for count, char in enumerate(x):
-                    if count % 4 == 1:
-                        if char == ' ':
-                            None
-                        else:
-                            nestedStack[currentStack].append(char)
-                        currentStack += 1
-
-                None
-    
-    stackOutput = ''
-    for x in range(len(nestedStack)):
-        if len(nestedStack[x]) != 0:
-            stackOutput += nestedStack[x][0]
     return stackOutput
 
-    # return nestedStack
-
 ##### PART 1/2 OUTPUT #####
-print("Part one answer: " + stackSortOne())
-print("Part two answer: " + stackSortTwo())
+print("Part one answer: " + stackSort(0))
+print("Part two answer: " + stackSort(1))
